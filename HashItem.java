b42848379ed6bd02465c4537e569b64b85d46919
Chuong Dang <83805611+JunnDang@users.noreply.github.com>
2025-01-10 22:45:06 -0800

    Add files via upload

---
+++ HashItem.java
@@ -0,0 +1,20 @@
+package Hashing;
+/**
+ * @author Chuong Dang
+ * Key is the key of the HashItem
+ * Value is the value being paired with the key
+ */
+public class HashItem<Key, Value> {
+    Key key;
+    Value value;
+
+    /**
+     * 
+     * @param key of the HashItem
+     * @param value the value being paired with the key
+     */
+    public HashItem(Key key, Value value){
+        this.key = key;
+        this.value = value;
+    }
+}
